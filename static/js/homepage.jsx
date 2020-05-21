1ef502135f869d1baf2b5e35c1f085efb40a844a
Chantel Yip <chantelyip422@gmail.com>
2020-05-21 16:31:02 -0700

    added welcome message to the user, link to the /cards page, nice large image of Balloonicorn

--- static/js/homepage.jsx
+++ static/js/homepage.jsx
@@ -4,10 +4,12 @@ class Homepage extends React.Component {
   render() {
     return (
       <div>
-        This component needs to be implemented.
+        <h1>Hooray! Welcome to Balloonicorn's Website!</h1>
+        <a href= "/cards"> View Cards here </a>
+        <img href="/static/img/balloonicorn.jpg"/>
       </div>
     );
   }
 }
 
-ReactDOM.render(<Homepage />, document.querySelector('#app'));
+ReactDOM.render(<Homepage />, document.getElementbyID('app'));
