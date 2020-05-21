54d53c2f48e616f02f9e5b5ef2052a4cdc1750f6
Chantel Yip <chantelyip422@gmail.com>
2020-05-21 22:37:27 +0000

    initial commit

---
+++ static/js/homepage.jsx
@@ -0,0 +1,13 @@
+"use strict";
+
+class Homepage extends React.Component {
+  render() {
+    return (
+      <div>
+        This component needs to be implemented.
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<Homepage />, document.querySelector('#app'));
