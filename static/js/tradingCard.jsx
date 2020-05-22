4ee2bd80db9899cfecc59905f2fd165f3b2efcea
Chantel Yip <chantelyip422@gmail.com>
2020-05-22 16:33:41 -0700

    tidied up the trading cards into a function to loop over each card

--- static/js/tradingCard.jsx
+++ static/js/tradingCard.jsx
@@ -1,79 +1,71 @@
+const tradingCardData = [
+  {
+    name: 'Balloonicorn',
+    skill: 'video games',
+    imgUrl: '/static/img/balloonicorn.jpg'
+  },
+
+  {
+    name: 'Float',
+    skill: 'baking pretzels',
+    imgUrl: '/static/img/float.jpg'
+  },
+
+  {
+    name: 'Llambda',
+    skill: 'knitting scarves',
+    imgUrl: '/static/img/llambda.jpg'
+  },
+
+
+  {
+    name: 'Off-By-One',
+    skill: 'climbing mountains',
+    imgUrl: '/static/img/off-by-one.jpg'
+  },
+
+  {
+    name: 'seedpy',
+    skill: 'cooking',
+    imgUrl: '/static/img/seedpy.jpg'
+  },
+
+  {
+    name: 'merge',
+    skill: 'driving',
+    imgUrl: '/static/img/merge.jpg'
+  },
+
+  {
+    name: 'polymorphism',
+    skill: 'going to class',
+    imgUrl: '/static/img/polymorphism.jpg'
+  }
+];
+
 "use strict";
 
 class TradingCard extends React.Component {
   render() {
+
+    const tradingCards = [];
+
+    for (const currentCard of tradingCardData) {
+      tradingCards.push(
+        <TradingCard
+        name={currentCard.name}
+        skill={currentCard.skill}
+        imageUrl={currentCard.imageUrl})
+        />
+      );
+    }
+
     return (
-      <div className="card">
-        <h2>Name: {this.props.name}</h2>
-        <img src={this.props.imgUrl} />
-        <h2>Skill: {this.props.skill} </h2>
+      <div>
+      {tradingCards}
       </div>
     );
   }
 }
 
-ReactDOM.render(
-  (
-    <TradingCard
-      name="Balloonicorn"
-      skill="video games"
-      imgUrl="/static/img/balloonicorn.jpg"
-    />
-  ),
-  document.querySelector('#balloonicorn')
-);
-
-ReactDOM.render(
-  (
-    <TradingCard
-      name="Float"
-      skill="baking pretzels"
-      imgUrl="/static/img/float.jpg"
-    />
-  ),
-  document.querySelector('#float')
-);
-
-ReactDOM.render(
-  (
-    <TradingCard
-      name="Llambda"
-      skill="knitting scarves"
-      imgUrl="/static/img/llambda.jpg"
-    />
-  ),
-  document.querySelector('#llambda')
-);
-
-ReactDOM.render(
-  (
-    <TradingCard
-      name="seedpy"
-      skill="cooking"
-      imgUrl="/static/img/seedpy.jpg"
-    />
-  ),
-  document.querySelector('#seedpy')
-);
-
-ReactDOM.render(
-  (
-    <TradingCard
-      name="merge"
-      skill="driving"
-      imgUrl="/static/img/merge.jpg"
-    />
-  ),
-  document.querySelector('#merge')
-);
-
-ReactDOM.render(
-  (
-    <TradingCard
-      name="polymorphism"
-      skill="going to class"
-      imgUrl="/static/img/polymorphism.jpg"
-    />
-  ),
-  document.querySelector('#polymorphism')
-);
+ 
